da371ba59ebc5f0b8fa340a87c3c784f4301efd3
FreezeSnail <smogonv3@gmail.com>
2020-06-30 14:38:58 -0400

    monster data generator tool

---
+++ Ardumon2/tools/monsterGenerator.py
@@ -0,0 +1,26 @@
+import csv
+
+print("#pragma once")
+
+with open('data/monstersheet', newline='') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter='\t', quotechar='|')
+    for row in reader:
+        print("static monsterSeed_t mon",row['id'], ' = { ', sep='',end ='')
+        print('0b', end ='')
+        print(bin(int(row['type']))[2:].zfill(3), end ='')
+        print(bin(int(row['id']))[2:].zfill(5), end =', ')
+        print('0b', end ='')
+        print(bin(int(row['atk']))[2:].zfill(4), end ='')
+        print(bin(int(row['def']))[2:].zfill(4), end ='')
+        print(bin(int(row['hp'] ))[2:].zfill(4), end ='')
+        print(bin(int(row['spd']))[2:].zfill(4), end =', ')
+        print('0b', end ='')
+        print(row['1'], end ='')
+        print(row['2'], end ='')
+        print(row['3'], end ='')
+        print(row['4'], end ='')
+        print(row['5'], end ='')
+        print(row['6'], end ='')
+        print(row['7'], end ='')
+        print(row['8'], end ='')
+        print(" };")
